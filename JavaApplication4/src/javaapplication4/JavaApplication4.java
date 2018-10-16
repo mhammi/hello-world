1e2955c5c95ad57976a6574869ab81fbbb0c3cf1
Hamm, Michael <hamm.m@zdf.de>
2018-10-16 17:16:07 +0200

    Test 181016_001

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -17,7 +17,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Yupp hier bin ich!");
+        System.out.println("YuppYupp hier bin ich!");
         System.out.println("Um: " + new Date());
         System.out.println("On: " + System.getProperty("java.version"));
     }
